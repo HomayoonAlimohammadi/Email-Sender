2a89932abeaaf29daa6673456069662ce641ff20
Homayoon Alimohammadi <homayoonalimohammadi@gmail.com>
2022-11-03 19:45:07 +0330

    Add threading, and README.md

--- python/README.md
+++ python/README.md
@@ -1,11 +1,36 @@
 # Send Customizable Emails to your desired recipients!
-## How does it work:
-- in the <b>professors.xlsx</b> insert all the records of the professors you desire!
-- don't forget to include all the main fields so that the email won't face any difficulties.
-- instead of the <b>Resume.pdf</b> put your own resume and it will be <b>Attached</b> to each and every email!
-- in the <b>gmail_sender.py</b> edit the <b>mail_content</b> variable to edit the main email body.
-- in the <b>variables.json</b> edit the variables you are going to implement in the email text.
-- Voila! Run the program and enjoy automting the daunting task of sending numerous emails to professors!<br><br>
-### Sample Email:  
-![Sample Email](https://github.com/TurtlePazzo/Email-Sender/blob/main/Sample%20Image.png?raw=true)
 
+## How does it work:
+
+### 1. Create mandatory files
+
+- In the project's `root directory` (where the `run_python.sh` exists) create two files <b>`my_data.json`</b> and <b>`professors.xlsx`</b>
+  in the <b>`my_data.json`</b>, just like the my_data_fake.json put all your specific data
+- <b>`professors.xlsx`</b> should contain all the data of each professor, just like a typical excel file.
+- <span style="color:yellow">WARNING</span>: put <b>`app password`</b> of your Gmail in the <b>`password`</b> field in the json file.
+- <span style="color:yellow">WARNING</span>: FILL ALL THE FIELDS IN BOTH FILES COMPLETELY OR YOU MIGHT ENCOUNTER SEVERAL ERRORS
+- In the `./python` directory (where this `README.md` exists) create a file named <b>`.env`</b> and write down your project's specific configuration
+- <span style="color:yellow">WARNING</span>: watch out for typos or mispelling in your path and config, this .env file is very sensetive
+- Put your resume with the <span style="color:red">EXACT</span> name of <b>`Resume.pdf`</b> in the root directory (where the `run_python.sh` exists)
+- Head to the `python` folder (where this `README.md` exists) and install the dependencies as follows:
+
+```shell
+cd python
+sudo apt install python3-pip python3-venv
+$(which python3) pip install -r requirements.txt
+source env/bin/activate
+```
+
+### 2. Run the Python Version
+
+- In the root of the project, run:
+
+```shell
+chmod u+x run_python.sh
+./run_python.sh
+```
+
+- if <b>`SEND_EMAIL`</b> is set to `1` in the `.env` config file, all the professors mentioned in the `professors.xlsx` file will get your email
+- if <b>`EXPORT_CONTENT`</b> is set to `1` in the `.env` config file, all the email contents will be exported in `./python/exported/` directory which . refers to the root of the project
+
+### 3. Enjoy!
--- python/email_sender.py
+++ python/email_sender.py
@@ -6,12 +6,15 @@ import os
 import sys
 import json
 import pandas as pd
+import threading
 
 from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
 from email.mime.text import MIMEText
 from typing import Any
 from dotenv import load_dotenv
+from html2text import html2text
+from time import time
 
 
 class EmailSender:
@@ -33,6 +36,9 @@ class EmailSender:
             raise EmailSenderInitializationException
 
     def quit_session(self) -> None:
+        """
+        Makes sure the session is shutdown successfully.
+        """
         try:
             self.session.quit()
         except Exception as _:
@@ -78,12 +84,16 @@ def get_email_subject_with_interest(base_subject: str, interest: str) -> str:
     return base_subject.format(interest=interest)
 
 
-def get_email_content(email_content_path: str, my_data: Any, prof_data: Any) -> str:
+def get_email_content(
+    email_content_path: str, my_data: Any, prof_data: Any, export: bool = False
+) -> str:
     """
     Given the path to the `email_content` template, renders it with the given data and returns the final `string`
     - Parameters:
         - `email_content_path`: path to the email_content template
+        - `my_data`: a key-value structure needed to render the template
         - `prof_data`: a key-value structure needed to render the template
+        - `export`: if `True`, each email content will be exported with a unique name in ./exported folder
     """
     with open(email_content_path, "r") as f:
         raw_content = f.read()
@@ -106,7 +116,31 @@ def get_email_content(email_content_path: str, my_data: Any, prof_data: Any) -> 
         target_degree=prof_data["target_degree"],
         prof_university=prof_data["university"],
     )
+
+    if export:
+        file_name = f"{prof_data['university']}_{prof_data['first_name'][0]}_{prof_data['last_name']}.txt"
+        export_email_content(email_content=email_content, file_name=file_name)
+
     return email_content
+
+
+def export_email_content(email_content: str, file_name: str) -> None:
+    """
+    Export email content to a .txt file in ./exoprted/
+    - Parameters:
+        - `email_content`: the actual text file content
+        - `file_name`: the name of file
+    - Path will be generated if not exists
+    """
+    current_dir = os.path.join(os.getcwd(), "python")
+    if not os.path.exists(os.path.join(current_dir, "exported")):
+        os.mkdir(os.path.join(current_dir, "exported"))
+    path = os.path.join(os.getcwd(), "python", "exported", file_name)
+    try:
+        with open(path, "w") as f:
+            f.write(html2text(email_content))
+    except Exception as e:
+        print(f"\033[1;33m WARNING: unable to export email content: {path}")
 
 
 def get_email_attachment(path: str, file_name: str = "") -> MIMEApplication:
@@ -168,32 +202,61 @@ def send_email_to_all_profs(
     attachment: MIMEApplication,
     my_data: Any,
     all_profs_data: pandas.DataFrame,
+    export_all_email_content: bool = False,
+    confirm_send: bool = True,
 ) -> None:
+    """
+    Handles the whole process of creating an email and exporting the content
+    - Parameters:
+        - `email_sender`: an instance of EmailSender to handle the SMTP session and mailing process
+        - `base_subject`: a template subject which the actual template gets rendered from
+        - `email_content_path`: path to the email content template which will get rendered finally
+        - `attachment`: an instance of MIMEApplication to attach to the email
+        - `my_data`: a key-value structure to render template from
+        - `all_profs_data`: a pandas.DataFrame to iterate on and extract individual `profs_data`
+        - `export_all_email_content`: if True, every email's content will be exported
+        - `'confirm_send`: if False, no email will be actually sent. Works separately with `export_all_email_content`
+    """
+    threads: list[threading.Thread] = list()
     for _, prof_data in all_profs_data.iterrows():
-        email_content = get_email_content(email_content_path, my_data, prof_data)
+        email_content = get_email_content(
+            email_content_path=email_content_path,
+            my_data=my_data,
+            prof_data=prof_data,
+            export=export_all_email_content,
+        )
         subject = get_email_subject_with_interest(base_subject, prof_data["interest"])
+        if not confirm_send:
+            continue
         try:
-            email_sender.create_and_send_email_message(
-                to=prof_data["email"],
-                subject=subject,
-                email_content=email_content,
-                attachment=attachment,
+            t = threading.Thread(
+                target=email_sender.create_and_send_email_message,
+                args=(prof_data["email"], subject, email_content, attachment),
             )
+            threads.append(t)
+            t.start()
         except Exception as e:
             raise SendEmailException
 
+        for t in threads:
+            t.join()
+
 
 def main():
+
+    # Start the Timer
+    t0 = time()
 
     load_dotenv()
     MY_DATA_PATH = os.environ.get("MY_DATA_PATH", "")
     EMAIL_CONTENT_PATH = os.environ.get("EMAIL_CONTENT_PATH", "")
     ALL_PROFS_DATA_PATH = os.environ.get("ALL_PROFS_DATA_PATH", "")
     EMAIL_ATTACHMENT_PATH = os.environ.get("EMAIL_ATTACHMENT_PATH", "")
+    SEND_EMAIL = os.environ.get("SEND_EMAIL", "1") == "1"
+    EXPORT_CONTENT = os.environ.get("EXPORT_CONTENT", "1") == "1"
     BASE_EMAIL_SUBJECT = (
         "Prospective student interested in {interest} with Machine Learning background"
     )
-    print(MY_DATA_PATH, ALL_PROFS_DATA_PATH, EMAIL_ATTACHMENT_PATH, EMAIL_CONTENT_PATH)
 
     my_data = load_my_data(MY_DATA_PATH)
     all_profs_data = load_all_profs_data(ALL_PROFS_DATA_PATH)
@@ -220,11 +283,17 @@ def main():
             attachment=attachment,
             my_data=my_data,
             all_profs_data=all_profs_data,
+            export_all_email_content=EXPORT_CONTENT,
+            confirm_send=SEND_EMAIL,
         )
     except SendEmailException as e:
         print("\033[0;31m error sending email to professors\n", e)
     finally:
         email_sender.quit_session()
+
+    # Print sending results:
+    t1 = time()
+    print(f"\033[0;36m Elapsed time: {round(t1 - t0, 2)} seconds.")
 
 
 if __name__ == "__main__":
